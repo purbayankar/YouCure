14b6d2c906f709505dfdd9eb9527324446cf4902
Swagnick99 <sagnikbasu19@gmail.com>
2021-08-18 18:17:34 +0530

    Added email check

--- Health_Care_Unit/health_auth.py
+++ Health_Care_Unit/health_auth.py
@@ -1,6 +1,8 @@
 from flask import Flask, Blueprint, render_template, url_for, request, redirect
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import login_user, logout_user, login_required
+from validate_email import validate_email
+
 from .models import Patient
 from . import db
 
@@ -11,11 +13,14 @@ def signup():
     if request.method == 'POST':
         name = request.form.get('first') +" "+ request.form.get('last')
         email = request.form.get('useremail')
+        is_valid = validate_email(email_address=email, check_format=True, check_blacklist=True, check_dns=True, check_smtp=False, smtp_debug=False)
         pwd = request.form.get('userpass')
 
         patient = Patient.query.filter_by(email=email).first()
         if patient:
             return render_template("signup.html", alert=True)
+        elif not is_valid:
+            return render_template("signup.html", unsafe=True)
 
         new_patient = Patient(name=name, email=email, pwd=generate_password_hash(pwd, method="sha256"))
         db.session.add(new_patient)
@@ -23,7 +28,7 @@ def signup():
 
         return redirect(url_for('health_auth.login'))
     
-    return render_template("signup.html", alert=False)
+    return render_template("signup.html")
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
